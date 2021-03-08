c5f361a01dc76463b149a2e5c16a68b3401ce50f
x0rnn <dofrasuphu@memsg.top>
2021-03-08 11:11:11 +0100

    Upload files to 'examples'

---
+++ examples/disableenablesmoke.lua
@@ -0,0 +1,36 @@
+disablesmoke = false
+
+function et_ClientSpawn(clientNum,revived)
+	if disablesmoke == true then
+		if et.gentity_get(clientNum,"sess.latchPlayerType") == 4 then
+			et.gentity_set(clientNum,"ps.ammoclip",30,0)
+			et.gentity_set(clientNum,"ps.ammo",30,0)
+		end
+	end
+end
+
+Vote:new("disablesmoke")
+	:description("Disable covert smoke grenades")
+	:vote(function()
+		if disablesmoke == true then
+			return false, "Covert smoke grenades are already disabled."
+		end
+
+		return string.format("Disable covert smoke grenades?")
+	end)
+	:pass(function()
+		disablesmoke = true
+	end)
+
+Vote:new("enablesmoke")
+	:description("Enable covert smoke grenades")
+	:vote(function()
+		if disablesmoke == false then
+			return false, "Covert smoke grenades are already enabled."
+		end
+
+		return string.format("Enable covert smoke grenades?")
+	end)
+	:pass(function()
+		disablesmoke = false
+	end)
