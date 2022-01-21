04417a4bc9e15e8e3ece5f39164204d6ae151705
x0rnn <c0rnn@live.com>
2022-01-21 20:06:04 +0100

    Update extend_time.lua

--- examples/extend_time.lua
+++ examples/extend_time.lua
@@ -9,7 +9,6 @@ stuck_time = 0
 intervals = {[1]=0, [2]=0}
 
 function et_RunFrame(levelTime)
-	if mapstarted == true and paused == false then return end
 	if math.mod(levelTime, 1000) ~= 0 then return end
 
 	local gamestate = tonumber(et.trap_Cvar_Get("gamestate"))
